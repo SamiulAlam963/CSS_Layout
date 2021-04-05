c44ac7b5473f406db91d11d4bf49085fb0cf922d
Samiul Alam <samiulalam963@gmail.com>
2021-04-05 19:59:07 +0600

    First commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>My website</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	
+	<body>
+		<div id="contain">
+			
+			<div id = "header">
+				<h1>Welcome to Info</h1>
+			</div>
+			
+			<div id="content">
+				<div id="navi">
+					<h3>Info</h3>
+					<ul>
+						<li>Name</li>
+						<li>Password</li>
+						<li>Email</li>
+					</ul>
+				</div>
+				
+				<div id="main">
+					<h2>This is Home page</h2>
+					<p>Welcome to CSS design.</p>
+				</div>
+			
+			</div>
+			
+			<div id="foot">
+				<p>Stay Connected</p>
+			</div>
+			
+		</div>
+	</body>
+	
+</html>
